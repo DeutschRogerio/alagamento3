13156eea057e721c7754c05f335ff1cc40062815
euforo <devrafaelvieira@gmail.com>
2022-11-27 17:18:27 -0300

    3%

--- enchente/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Area_interesse(models.Model):
-    """Classe Areas de interesse de notificação"""
-    name = models.CharField(max_length=200, help_text='Adicione o nome do bairro que gostaria de receber notificações')
-
-    def __str__(self):
-        """String for representing the Model object."""
-        return self.name
