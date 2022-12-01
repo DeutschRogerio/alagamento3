f0189f5611b085bcba0974f857af2e4b1e6e93f9
euforo <devrafaelvieira@gmail.com>
2022-12-01 20:41:26 -0300

    20%

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__, template_folder='static')
 
 @app.route("/teste1")
 def index():
-  return render_template('Cadastro_incidentes.html')
+  return render_template('Cadastro_incidente.html')
 
 @app.route("/teste2")
 def cadastro_incidentes():
-  return render_template('Cadastro_incidentes.html')
+  return render_template('Cadastro_pessoa.html')
 
 
 if __name__ == "__main__":
